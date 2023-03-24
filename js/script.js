623be3342d4ab0adaeb729b9107a54a9370802ce
Vladislav Semikhat <114361196+d1ksi@users.noreply.github.com>
2023-03-24 12:00:57 +0200

    1234
    1234

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+   // Optional parameters
+   loop: false,
+
+   // Navigation arrows
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+   },
+});
+
